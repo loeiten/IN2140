47cbc84e40941e6cf2b17c095112c6e71acdbbf6
Michael Løiten <michael.l.magnussen@gmail.com>
2023-02-10 13:26:20 +0100

    Working assignment 1

--- programming_assignments/set_1/assignment_1.c
+++ programming_assignments/set_1/assignment_1.c
@@ -1,4 +1,6 @@
+#include <ctype.h>
 #include <libgen.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,6 +9,10 @@
 // https://stackoverflow.com/questions/2115301/where-command-line-arguments-are-stored
 // https://stackoverflow.com/questions/39626689/how-much-memory-is-allocated-for-argv?noredirect=1&lq=1
 int main(int argc, char** argv) {
+  int i;
+  int len;
+  int number;
+  int strBufferLen;
   char* msg;
 
   if (argc < 2) {
@@ -15,20 +21,31 @@ int main(int argc, char** argv) {
     return EXIT_FAILURE;
   }
 
+  // Check if we are passed an unsigned integer
+  len = strlen(argv[1]);
+  i = 0;
+  number = 0;
+
+  while ((isdigit(argv[1][i])) && (i < len)) {
+    number *= 10;
+    number += argv[1][i] - '0';
+    ++i;
+  }
+
+  if (i == len) {
+    printf("Number: %d", number);
+    return EXIT_SUCCESS;
+  }
+
   // Allocate to the heap instead of stack to protect against buffer overflow
   // +1 to account for the \0 terminator
-  msg = (char*)malloc((strlen(argv[1]) + 1) * sizeof(char));
-
-  if (msg == NULL) {
-    free(msg);
-    printf("Memory not allocated\n");
-    return EXIT_FAILURE;
-  }
+  strBufferLen = len + 1;
+  msg = (char*)malloc(strBufferLen * sizeof(char));
 
   // Almost always, snprintf is better than strcpy according to cpplint
   // Also see:
   // https://stackoverflow.com/questions/2606539/snprintf-vs-strcpy-etc-in-c
-  snprintf(msg, sizeof(msg), "%s", argv[1]);
+  snprintf(msg, strBufferLen, "%s", argv[1]);
   printf("Input: %s\n", msg);
 
   free(msg);
