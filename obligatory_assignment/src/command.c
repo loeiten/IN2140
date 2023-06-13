122e1a2c6710fa52b6657c87a94f94ec4a931630
Michael Løiten <michael.l.magnussen@gmail.com>
2023-06-13 20:43:50 +0200

    Enhance security

--- obligatory_assignment/src/command.c
+++ obligatory_assignment/src/command.c
@@ -1,13 +1,11 @@
-#include "../include/command.h"
-
-#include <stddef.h>  // for NULL, size_t
-#include <stdio.h>   // for perror, snprintf
+#include <stdio.h>   // for fprintf, perror, NULL, size_t, snprintf, stderr
 #include <stdlib.h>  // for free, malloc, EXIT_FAILURE, EXIT_SUCCESS
 #include <string.h>  // for strlen, strtok_r, strcmp
 
 int getCommand(const char *commandStr, char **command, char ***args,
                size_t *nArgs) {
   size_t strLen;
+  int charWritten;  // Number of chars not including terminating char
   char *token;
   char *savePtr;
   const char *format = "%s";
@@ -19,7 +17,12 @@ int getCommand(const char *commandStr, char **command, char ***args,
     perror("Could not allocate memory to commandStrCpy: ");
     return EXIT_FAILURE;
   }
-  snprintf(commandStrCpy, (strLen + 1), "%s", commandStr);
+  charWritten = snprintf(commandStrCpy, (strLen + 1), "%s", commandStr);
+  if ((charWritten < 0) || (charWritten > strLen)) {
+    free(commandStrCpy);
+    fprintf(stderr, "Failed to copy the commandStr\n");
+    return EXIT_FAILURE;
+  }
 
   // Count the number of arguments
   size_t nSpaces = 0;
@@ -36,13 +39,24 @@ int getCommand(const char *commandStr, char **command, char ***args,
   // NOTE: strtok_r is the thread safe version of strtok
   token = strtok_r(commandStrCpy, " ", &savePtr);
   strLen = snprintf(NULL, 0, format, commandStr);
+  if (strLen < 1) {
+    free(commandStrCpy);
+    fprintf(stderr, "Failed to get strLen of commandStr\n");
+    return EXIT_FAILURE;
+  }
   *command = (char *)malloc((strLen + 1) * sizeof(char));
   if (*command == NULL) {
     free(commandStrCpy);
     perror("Could not allocate memory to command: ");
     return EXIT_FAILURE;
   }
-  snprintf(*command, strLen + 1, "%s", token);
+  charWritten = snprintf(*command, strLen + 1, "%s", token);
+  if ((charWritten < 0) || (charWritten > strLen)) {
+    free(commandStrCpy);
+    free(*command);
+    fprintf(stderr, "Failed to copy the command\n");
+    return EXIT_FAILURE;
+  }
 
   // Special case for set_model
   if (strcmp(*command, "setModel") == 0) {
@@ -52,7 +66,7 @@ int getCommand(const char *commandStr, char **command, char ***args,
     *args = (char **)malloc(*nArgs * sizeof(char *));
     if (**args == NULL) {
       free(commandStrCpy);
-      free(command);
+      free(*command);
       perror("Could not allocate memory to args: ");
       return EXIT_FAILURE;
     }
@@ -63,12 +77,20 @@ int getCommand(const char *commandStr, char **command, char ***args,
     (*args)[0] = (char *)malloc((strLen + 1) * sizeof(char));
     if (args[0] == NULL) {
       free(commandStrCpy);
-      free(command);
-      free(args);
+      free(*command);
+      free(*args);
       perror("Could not allocate memory to args[0]: ");
       return EXIT_FAILURE;
     }
-    snprintf((*args)[0], strLen + 1, "%s", token);
+    charWritten = snprintf((*args)[0], strLen + 1, "%s", token);
+    if ((charWritten < 0) || (charWritten > strLen)) {
+      free(commandStrCpy);
+      free(*command);
+      free((*args)[0]);
+      free(*args);
+      fprintf(stderr, "Failed to copy to arg[0]\n");
+      return EXIT_FAILURE;
+    }
 
     // The rest of the string is the model name and can contain spaces
     // -2 for the spaces which are not captured in commandStr and command
@@ -77,13 +99,22 @@ int getCommand(const char *commandStr, char **command, char ***args,
     if (args[1] == NULL) {
       free(commandStrCpy);
       free(*command);
-      free(args[0]);
-      free(args);
+      free((*args)[0]);
+      free((*args));
       perror("Could not allocate memory to args[1]: ");
       return EXIT_FAILURE;
     }
-    snprintf((*args)[1], strLen, "%s",
-             &commandStr[strlen(commandStr) - strLen]);
+    charWritten = snprintf((*args)[1], strLen, "%s",
+                           &commandStr[strlen(commandStr) - strLen]);
+    if ((charWritten < 0) || (charWritten > strLen)) {
+      free(commandStrCpy);
+      free(*command);
+      free((*args)[0]);
+      free((*args)[1]);
+      free((*args));
+      fprintf(stderr, "Failed to copy to arg[1]\n");
+      return EXIT_FAILURE;
+    }
 
     // Free the copy
     free(commandStrCpy);
@@ -101,15 +132,25 @@ int getCommand(const char *commandStr, char **command, char ***args,
     (*args)[i] = (char *)malloc((strLen + 1) * sizeof(char));
     if (args[i] == NULL) {
       free(commandStrCpy);
-      free(command);
+      free(*command);
       for (size_t j = 0; j < i; ++j) {
-        free(args[j]);
+        free((*args)[j]);
       }
-      free(args);
+      free(*args);
       perror("Could not allocate memory to args[i]: ");
       return EXIT_FAILURE;
     }
-    snprintf((*args)[i], strLen + 1, "%s", token);
+    charWritten = snprintf((*args)[i], strLen + 1, "%s", token);
+    if ((charWritten < 0) || (charWritten > strLen)) {
+      free(commandStrCpy);
+      free(*command);
+      for (size_t j = 0; j <= i; ++j) {
+        free((*args)[j]);
+      }
+      free(*args);
+      fprintf(stderr, "Failed to copy to arg[i]\n");
+      return EXIT_FAILURE;
+    }
   }
 
   return EXIT_SUCCESS;
