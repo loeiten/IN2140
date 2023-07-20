c4004ad2ff519da3beb2acb142ae1627f934e380
Michael Løiten <michael.l.magnussen@gmail.com>
2023-07-20 12:43:36 +0200

    Add findRouterId test

--- obligatory_assignment/test/test_router.c
+++ obligatory_assignment/test/test_router.c
@@ -1,11 +1,12 @@
 
+#include <assert.h>  // for assert
 #include <libgen.h>  // for basename
 #include <stddef.h>  // for NULL, size_t
 #include <stdio.h>   // for fprintf, perror, stderr, snprintf
-#include <stdlib.h>  // for free, atoi, malloc, EXIT_FAILURE, EXI...
+#include <stdlib.h>  // for free, atoi, malloc, EXIT_SUCCESS, EXI...
 #include <string.h>  // for strcmp, strlen
 
-#include "../include/router.h"  // for printNeighbors, MAX_NEIGHBORS
+#include "../include/router.h"  // for findRouterId, printNeighbors, Router
 
 void testPrintNeighbors(const char *const neighborStr) {
   // Initialize test
@@ -75,6 +76,36 @@ void testPrintNeighbors(const char *const neighborStr) {
   return;
 }
 
+void testFindRouterId() {
+  // Create testRouters
+  unsigned int N = 3;
+  struct Router router0 = {.routerId = 42};
+  struct Router router1 = {.routerId = 7};
+  struct Router router2 = {.routerId = 88};
+  struct Router routerArray[3] = {router0, router1, router2};
+
+  int hitIdx;
+
+  // Test positive
+  int success = findRouterId(routerArray, N, 42, &hitIdx);
+  assert(success == EXIT_SUCCESS);
+  assert(hitIdx == 0);
+
+  success = findRouterId(routerArray, N, 88, &hitIdx);
+  assert(success == EXIT_SUCCESS);
+  assert(hitIdx == 2);
+
+  success = findRouterId(routerArray, N, 7, &hitIdx);
+  assert(success == EXIT_SUCCESS);
+  assert(hitIdx == 1);
+
+  success = findRouterId(routerArray, N, 100, &hitIdx);
+  assert(success == EXIT_FAILURE);
+  assert(hitIdx == -1);
+
+  return;
+}
+
 int main(int argc, char **argv) {
   if (argc < 2) {
     // NOTE: Base is from POSIX.1-2008, not the C-standard, see
@@ -88,6 +119,8 @@ int main(int argc, char **argv) {
 
   if (strcmp(argv[1], "printNeighbors") == 0) {
     testPrintNeighbors(argv[2]);
+  } else if (strcmp(argv[1], "findRouterId") == 0) {
+    testFindRouterId();
   } else {
     fprintf(stderr, "No test named %s in %s\n", argv[1], basename(argv[0]));
   }
