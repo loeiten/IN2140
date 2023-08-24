0105dc59d3be62d9b13ef8331bdd68ba33bb81a9
Michael Løiten <michael.l.magnussen@gmail.com>
2023-08-24 11:41:46 +0200

    Implement Dijkstra using shortest path three

--- home_exam/routing_server/include/dijkstra.h
+++ home_exam/routing_server/include/dijkstra.h
@@ -5,10 +5,27 @@
  * @brief Computes the distance from the source to all other nodes.
  *
  * @param src The id of the source
- * @param graph The graph to traverse given as an adjacency matrix
+ * @param graph The graph to traverse given as an adjacency matrix.
+ *              The adjacency matrix will have INT_MAX where if there are no
+ *              connection between the nodes
  * @param distance The distance from src to all other nodes
+ * @param n The number of nodes in the graph
+ * @return int 0 on success, 1 on failure
  */
-void dijkstra(const int src, const int *const *const graph,
-              int *const *const distance);
+int dijkstra(const int src, const int *const *const graph,
+             int *const *const distance, const int n);
+
+/**
+ * @brief Helper which finds the index with the lowest distance from src
+ *
+ * @param distance The distance from src to all other nodes
+ * @param visited The array marking if a node is visited.
+ *                Note that in Dijkstra's algorithm a node is only marked
+ *                visited when it has the lowest total distance from the src
+ * @param n The number of nodes in the graph
+ * @return int The index of the node with the lowest distance to the src
+ */
+int getMinDistanceIdx(const int *const distance, const int *const visited,
+                      const int n);
 
 #endif  // HOME_EXAM_ROUTING_SERVER_INCLUDE_DIJKSTRA_H_
--- home_exam/routing_server/src/dijkstra.c
+++ home_exam/routing_server/src/dijkstra.c
@@ -1,9 +1,86 @@
 #include "../include/dijkstra.h"
 
-void dijkstra(const int src, const int *const *const graph,
-              int *const *const distance) {
-  (void)src;
-  (void)graph;
-  (void)distance;
-  return;
+#include <limits.h>
+#include <math.h>
+#include <stdio.h>  // for perror
+#include <stdlib.h>
+
+int getMinDistanceIdx(const int *const distance, const int *const visited,
+                      const int n) {
+  int minDist = INT_MAX;
+  int minIdx = -1;
+
+  for (int i = 0; i < n; ++i) {
+    // We will just search through non-visited nodes
+    // If the minIdx is to be updated, is must be less than any previously found
+    if ((visited[i] == 0) && (distance[i] <= minDist)) {
+      minDist = distance[i];
+      minIdx = i;
+    }
+  }
+
+  return minIdx;
+}
+
+int dijkstra(const int src, const int *const *const graph,
+             int *const *const distance, const int n) {
+  // Solve Dijkstra's algorithm using shortest path three
+  // Note that this solution has time complexity O(V^2) where V is the number of
+  // vertices
+  // By using a heap this can be reduced to O(E*log(V)) where E are the number
+  // of edges
+
+  // Allocate memory to the array of visited nodes
+  int *visited = (int *)malloc((n) * sizeof(int));
+  if (visited == NULL) {
+    perror("Could not allocate memory to visited: ");
+    return EXIT_FAILURE;
+  }
+  // Mark all nodes as unvisited
+  for (int i = 0; i < n; ++i) {
+    visited[i] = 0;
+  }
+
+  // First set the distance to every node to zero
+  for (int i = 0; i < n; ++i) {
+    (*distance)[n] = INT_MAX;
+  }
+  // The distance to itself is 0
+  (*distance)[src] = 0;
+
+  // As we will find the shortest path to all nodes, and since we visit a new
+  // node in each iteration, we need to iterate through n-1 nodes as we have
+  // already found the shortest path to the node itself
+  for (int iteration = 0; iteration < (n - 1); ++iteration) {
+    // Pick the minumum distance from the set of vertices not yet processed
+    int minIdx = getMinDistanceIdx(*distance, visited, n);
+    if (minIdx == -1) {
+      free(visited);
+      fprintf(stderr,
+              "Unexpected error: Could not find a minIdx for the %d iteration",
+              iteration);
+      return EXIT_FAILURE;
+    }
+
+    // Mark this node as visited
+    visited[minIdx] = 1;
+
+    for (int i = 0; i < n; ++i) {
+      if (
+          // If this node has not been visited
+          (visited[i] != 1) &&
+          // There must be an edge between minIdx and i
+          (graph[minIdx][i] != INT_MAX) &&
+          // A path from the source must have been registered
+          ((*distance)[minIdx] != INT_MAX) &&
+          // The new path must be smaller than any previously recorded
+          ((*distance)[minIdx] + graph[minIdx][i] < (*distance)[i])) {
+        // Update the shortest distance to the current node
+        (*distance)[i] = (*distance)[minIdx] + graph[minIdx][i];
+      }
+    }
+  }
+
+  free(visited);
+  return EXIT_SUCCESS;
 }
